1f0efcb5379ceda1fd7520541847c3bee7728602
Daniel Españadero <developer@daniel-espanadero.com>
2023-07-25 21:55:12 +0200

    First commit

---
+++ src/main/java/com/springsecurity/protectedpaths/ProtectedPathsApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurity.protectedpaths;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtectedPathsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtectedPathsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsecurity/protectedpaths/ProtectedPathsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.protectedpaths;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtectedPathsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
